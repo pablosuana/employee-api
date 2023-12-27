2c00c431c7f85fb401ab676272544a2b9f20b91f
Pablo Suana Cuesta <pablo.suana-cuesta@klarna.com>
2023-12-27 17:30:14 +0100

    Adding use case for creating records

---
+++ src/test/scala/useCases/CreateRecordTest.scala
@@ -0,0 +1,13 @@
+package useCases
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class CreateRecordTest extends AnyFunSuite with Matchers {
+
+  test(s"CreateRecord class has the expected methods") {
+
+//    val createRecord = CreateRecord()
+
+  }
+}
