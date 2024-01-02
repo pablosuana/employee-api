267b68b08ccd7128a5d92de71414c4d720642a29
Pablo Suana Cuesta <pablo.suana-cuesta@klarna.com>
2024-01-02 03:16:13 +0100

    Adding files to resources with app configuration

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE if not exists "employee_table" (
+    "id" varchar(50) NOT NULL,
+    "full_name" varchar(100) NOT NULL,
+    "email" varchar(300) NOT NULL,
+    "date_of_birth" varchar(10) NOT NULL,
+    "hobbies" varchar(600),
+    "created_at" timestamp NOT NULL DEFAULT now(),
+    "updated_at" timestamp NOT NULL DEFAULT now(),
+    PRIMARY KEY ("email", "created_at")
+    );
