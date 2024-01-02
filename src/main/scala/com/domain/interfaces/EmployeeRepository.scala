c2ef11e6a4e41441bf2427252c0c33a241f8cccb
Pablo Suana Cuesta <pablo.suana-cuesta@klarna.com>
2024-01-02 03:23:10 +0100

    Adding interfaces for repository

---
+++ src/main/scala/com/domain/interfaces/EmployeeRepository.scala
@@ -0,0 +1,29 @@
+package com.domain.interfaces
+
+import com.domain.Entities.EmployeeBase
+import com.domain.interfaces.db.{DbAsyncOperationsBase, DbConfigBase, DbConnectionProvider, DbResponse}
+
+import java.util.UUID
+import scala.concurrent.{ExecutionContext, Future}
+
+trait EmployeeRepository[DbResponseType <: DbResponse, EmployeeType <: EmployeeBase] {
+
+  type QueryToDbType
+  type DbConnectionType
+
+  implicit val ec: ExecutionContext
+
+  val dbConnectionConfig: DbConfigBase
+  val dbConnectionProvider: DbConnectionProvider[DbConnectionType]
+  val dbOperations: DbAsyncOperationsBase[DbConnectionType, QueryToDbType, DbResponseType]
+
+  def getAllEmployees: Future[Seq[EmployeeType]]
+
+  def getEmployeeById(id: UUID): Future[Option[DbResponseType]]
+
+  def createEmployee(employee: EmployeeType, createdAt: String): Future[Boolean]
+
+  def deleteEmployee(email: Option[String], id: Option[String]): Future[Boolean]
+
+  def updateEmployee(employee: EmployeeType, updatedAt: String): Future[Boolean]
+}
