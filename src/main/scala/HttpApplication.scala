f0d331efe2dc1ede60bca43c38c237f82438474e
Pablo Suana Cuesta <pablo.suana-cuesta@klarna.com>
2024-01-02 03:30:40 +0100

    Adding main app file

---
+++ src/main/scala/HttpApplication.scala
@@ -0,0 +1,30 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.RouteConcatenation._
+import com.typesafe.config.{Config, ConfigFactory}
+import infrastructure.endpoints.{CreateEmployeeEndpoint, DeleteEmployeeEndpoint, GetEmployeeEndpoint, UpdateEmployeeEndpoint}
+import infrastructure.interfaces.EmployeeRepositoryImplementation
+import org.slf4j.LoggerFactory
+import sttp.tapir.server.akkahttp.AkkaHttpServerInterpreter
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object HttpApplication extends App {
+
+  private val logger           = LoggerFactory.getLogger(getClass)
+  protected val config: Config = ConfigFactory.load("application.conf")
+  implicit val as: ActorSystem = ActorSystem()
+
+  val repository = new EmployeeRepositoryImplementation
+
+  val serviceInterface: Route =
+    AkkaHttpServerInterpreter()(as.dispatcher).toRoute(new CreateEmployeeEndpoint(repository).endpointToUse) ~
+    AkkaHttpServerInterpreter()(as.dispatcher).toRoute(new UpdateEmployeeEndpoint(repository).endpointToUse) ~
+    AkkaHttpServerInterpreter()(as.dispatcher).toRoute(new GetEmployeeEndpoint(repository).endpointToUse) ~
+    AkkaHttpServerInterpreter()(as.dispatcher).toRoute(new DeleteEmployeeEndpoint(repository).endpointToUse)
+
+  logger.info("Starting application")
+
+  Http().newServerAt("localhost", 8080).bindFlow(serviceInterface)
+}
